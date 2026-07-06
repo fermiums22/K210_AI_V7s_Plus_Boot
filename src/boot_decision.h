6c2aa9f53c3296fe3b95969f249ecd7e6636a7cf
fermiums22 <118602434+fermiums22@users.noreply.github.com>
2026-07-06 16:43:51 +0300

    Expose app header read status

--- src/boot_decision.h
+++ src/boot_decision.h
@@ -5,5 +5,6 @@
 
 extern uint32_t boot_decision_reset_status_raw;
 extern boot_app_header_t boot_decision_app_header;
+extern int boot_decision_app_header_read_rc;
 
 uint32_t boot_decision_get_reason(void);
