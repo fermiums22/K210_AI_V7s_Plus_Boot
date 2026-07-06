578e34b96ff560875a4228508e93a843dc902370
fermiums22 <118602434+fermiums22@users.noreply.github.com>
2026-07-06 20:19:01 +0300

    Expose boot flash erase and program APIs

--- src/boot_flash.h
+++ src/boot_flash.h
@@ -6,4 +6,6 @@
 int boot_flash_read(uint32_t flash_offset, void *dst, uint32_t len);
 int boot_flash_read_app_header(uint32_t slot_offset, boot_app_header_t *out);
 int boot_flash_load_app_image(const boot_app_header_t *hdr);
+int boot_flash_erase_range(uint32_t flash_offset, uint32_t len);
+int boot_flash_program(uint32_t flash_offset, const void *src, uint32_t len);
 uint32_t boot_flash_read_jedec_id(void);
