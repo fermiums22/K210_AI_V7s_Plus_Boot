b7494ede2a8bc9f4d548255d7fabe448bbda95da
fermiums22 <118602434+fermiums22@users.noreply.github.com>
2026-07-06 13:33:20 +0300

    Expose validated app header

--- src/boot_decision.h
+++ src/boot_decision.h
@@ -1,7 +1,9 @@
 #pragma once
 
 #include <stdint.h>
+#include "boot_config.h"
 
 extern uint32_t boot_decision_reset_status_raw;
+extern boot_app_header_t boot_decision_app_header;
 
 uint32_t boot_decision_get_reason(void);
