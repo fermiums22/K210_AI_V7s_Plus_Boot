286551cf0b5619e182b99e40ec3f282b786dbb54
fermiums22 <118602434+fermiums22@users.noreply.github.com>
2026-07-07 15:19:12 +0300

    Add boot SDK driver init wrapper

---
+++ src/boot_sdk_init.cpp
@@ -0,0 +1,14 @@
+#include "log.h"
+
+extern "C" void install_drivers();
+
+extern "C" void boot_sdk_install_drivers_once(void)
+{
+    static int installed = 0;
+    if (installed)
+        return;
+    installed = 1;
+    LOG("BOOT_SDK_INSTALL_DRIVERS");
+    install_drivers();
+    LOG("BOOT_SDK_INSTALL_DRIVERS_DONE");
+}
