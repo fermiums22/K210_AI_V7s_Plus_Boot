8182be409eac4d19082cf089532a77c0bb65b88a
v.taranov <v.taranov@encata.net>
2026-07-15 10:13:14 +0300

    Remove generated boot artifacts and fix SDK paths

--- build_v2/CMakeFiles/k210_boot_v2.dir/newlib_locks.c.obj.d
+++
@@ -1,9 +0,0 @@
-CMakeFiles/k210_boot_v2.dir/newlib_locks.c.obj: \
- D:\w_space\K210_AI_V7s_Plus_Boot\firmware_v2\newlib_locks.c \
- c:\k210\toolchain\kendryte-toolchain\riscv64-unknown-elf\include\sys\lock.h \
- c:\k210\toolchain\kendryte-toolchain\riscv64-unknown-elf\include\_ansi.h \
- c:\k210\toolchain\kendryte-toolchain\riscv64-unknown-elf\include\newlib.h \
- c:\k210\toolchain\kendryte-toolchain\riscv64-unknown-elf\include\_newlib_version.h \
- c:\k210\toolchain\kendryte-toolchain\riscv64-unknown-elf\include\sys\config.h \
- c:\k210\toolchain\kendryte-toolchain\riscv64-unknown-elf\include\machine\ieeefp.h \
- c:\k210\toolchain\kendryte-toolchain\riscv64-unknown-elf\include\sys\features.h
--- build_v2/freertos/CMakeFiles/freertos.dir/pthread.c.obj.d
+++
@@ -1,2 +0,0 @@
-freertos/CMakeFiles/freertos.dir/pthread.c.obj: \
- D:\w_space\K210_AI_V7s_Plus_Boot\lib\freertos\pthread.c
